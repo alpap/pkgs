3be93dfd5fd25c62362c3eee848dbce888f87ff0
Alexandros Papadopoulos <al.pap@live.com>
2024-09-15 22:15:46 +0200

    udpate

--- lib.sh
+++ lib.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+wget https://gitlab.com/-/project/31927362/uploads/1164b1c240e4cdacc652b3dd5953c3d1/nala_0.15.4_all.deb
 
 # gpgv
 
@@ -12,32 +12,52 @@
 
 # seccomp 242
 
-# wget http://http://launchpadlibrarian.net/723477741/gpgv_2.4.4-2ubuntu17_amd64.deb
-# gpgv_2.4.4-2ubuntu17_amd64.deb
+wget http://http://launchpadlibrarian.net/723477741/gpgv_2.4.4-2ubuntu17_amd64.deb
+dpkg -i gpgv_2.4.4-2ubuntu17_amd64.deb
+
+wget http://launchpadlibrarian.net/743869696/libapt-pkg6.0t64_2.8.2_amd64.deb
+dpkg -i ibapt-pkg6.0t64_2.8.2_amd64.deb
+
+wget http://launchpadlibrarian.net/699871466/ubuntu-keyring_2023.11.28.1_all.deb
+dpkg -i ubuntu-keyring_2023.11.28.1_all.deb
+
+wget http://launchpadlibrarian.net/742984273/libseccomp2_2.5.5-1ubuntu3.1_amd64.deb
+dpkg -i libseccomp2_2.5.5-1ubuntu3.1_amd64.deb
+
+function download {
+
+# wget http://launchpadlibrarian.net/722988751/pkexec_124-2ubuntu1_amd64.deb
+
+# sudo dpkg -i pkexec_124-2ubuntu1_amd64.deb
+
+}
+# wget http://launchpadlibrarian.net/722988751/pkexec_124-2ubuntu1_amd64.deb
+
+# sudo dpkg -i pkexec_124-2ubuntu1_amd64.deb
 
 
 
 
-wget http://launchpadlibrarian.net/723531705/python3-gi_3.48.2-1_amd64.deb
+# wget http://launchpadlibrarian.net/722988751/pkexec_124-2ubuntu1_amd64.deb
 
-sudo dpkg -i python3-gi_3.48.2-1_amd64.deb
+# sudo dpkg -i pkexec_124-2ubuntu1_amd64.deb
 
-wget http://launchpadlibrarian.net/722988751/pkexec_124-2ubuntu1_amd64.deb
+# wget http://launchpadlibrarian.net/723531705/python3-gi_3.48.2-1_amd64.deb
 
-sudo dpkg -i pkexec_124-2ubuntu1_amd64.deb
+# sudo dpkg -i python3-gi_3.48.2-1_amd64.deb
 
-wget http://launchpadlibrarian.net/747453547/gir1.2-vte-2.91_0.77.92-1_amd64.deb
+# wget http://launchpadlibrarian.net/747453547/gir1.2-vte-2.91_0.77.92-1_amd64.deb
 
-sudo dpkg -i gir1.2-vte-2.91_0.77.92-1_amd64.deb
+# sudo dpkg -i gir1.2-vte-2.91_0.77.92-1_amd64.deb
 
-wget http://launchpadlibrarian.net/746740141/gir1.2-gtk-3.0_3.24.43-3ubuntu2_amd64.deb
+# wget http://launchpadlibrarian.net/746740141/gir1.2-gtk-3.0_3.24.43-3ubuntu2_amd64.deb
 
-sudo dpkg -i gir1.2-gtk-3.0_3.24.43-3ubuntu2_amd64.deb
+# sudo dpkg -i gir1.2-gtk-3.0_3.24.43-3ubuntu2_amd64.deb
 
-wget http://launchpadlibrarian.net/742198164/gdebi-core_0.9.5.7+nmu9_all.deb
+# wget http://launchpadlibrarian.net/742198164/gdebi-core_0.9.5.7+nmu9_all.deb
 
-sudo dpkg -i gdebi-core_0.9.5.7+nmu9_all.deb
+# sudo dpkg -i gdebi-core_0.9.5.7+nmu9_all.deb
 
-wget http://launchpadlibrarian.net/742198166/gdebi_0.9.5.7+nmu9_all.deb
+# wget http://launchpadlibrarian.net/742198166/gdebi_0.9.5.7+nmu9_all.deb
 
-sudo dpkg -i gdebi_0.9.5.7+nmu9_all.deb
+# sudo dpkg -i gdebi_0.9.5.7+nmu9_all.deb
