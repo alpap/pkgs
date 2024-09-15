a47a467f34e2a1411cefe81cdb7c90786766f006
Alexandros Papadopoulos <al.pap@live.com>
2024-09-15 22:02:38 +0200

    udpate

--- boot_kernel.sh
+++ boot_kernel.sh
@@ -1,1 +1,42 @@
 
+#!/usr/bin/env bash
+
+sudo fdisk -l
+sudo mount /dev/sda2 /mnt
+sudo mount --bind /dev /mnt/dev
+sudo mount --bind /dev/pts /mnt/dev/pts
+sudo mount --bind /proc /mnt/proc
+sudo mount --bind /sys /mnt/sys
+sudo chroot /mnt
+
+
+
+
+
+# Start with a livecd, open a a terminal and execute:
+
+# sudo fdisk -l
+# sudo mount /dev/sdax /mnt
+# sudo mount --bind /dev /mnt/dev
+# sudo mount --bind /dev/pts /mnt/dev/pts
+# sudo mount --bind /proc /mnt/proc
+# sudo mount --bind /sys /mnt/sys
+# sudo chroot /mnt
+
+# If you /boot is on a separate partition also call:
+
+# sudo mount /dev/sday /mnt/boot
+
+# and now you can make update-initramfs and update-grub without errors.
+
+# update-initramfs -u -k 2.6.38-8-generic (or your version)
+
+# If you don't know your version. Use:
+
+# dpkg --list | grep linux-image
+
+# And just update Grub.
+
+# update-grub
+
+# Reboot your system.
---
+++ lib.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+
+# gpgv
+
+# libapt-pkg6.0t64 (>= 2.8.2)
+
+# ubuntu-keyring
+
+# libgnutls30t64 (>= 3.8.1)
+
+# seccomp 242
+
+# wget http://http://launchpadlibrarian.net/723477741/gpgv_2.4.4-2ubuntu17_amd64.deb
+# gpgv_2.4.4-2ubuntu17_amd64.deb
+
+
+
+
+wget http://launchpadlibrarian.net/723531705/python3-gi_3.48.2-1_amd64.deb
+
+sudo dpkg -i python3-gi_3.48.2-1_amd64.deb
+
+wget http://launchpadlibrarian.net/722988751/pkexec_124-2ubuntu1_amd64.deb
+
+sudo dpkg -i pkexec_124-2ubuntu1_amd64.deb
+
+wget http://launchpadlibrarian.net/747453547/gir1.2-vte-2.91_0.77.92-1_amd64.deb
+
+sudo dpkg -i gir1.2-vte-2.91_0.77.92-1_amd64.deb
+
+wget http://launchpadlibrarian.net/746740141/gir1.2-gtk-3.0_3.24.43-3ubuntu2_amd64.deb
+
+sudo dpkg -i gir1.2-gtk-3.0_3.24.43-3ubuntu2_amd64.deb
+
+wget http://launchpadlibrarian.net/742198164/gdebi-core_0.9.5.7+nmu9_all.deb
+
+sudo dpkg -i gdebi-core_0.9.5.7+nmu9_all.deb
+
+wget http://launchpadlibrarian.net/742198166/gdebi_0.9.5.7+nmu9_all.deb
+
+sudo dpkg -i gdebi_0.9.5.7+nmu9_all.deb
